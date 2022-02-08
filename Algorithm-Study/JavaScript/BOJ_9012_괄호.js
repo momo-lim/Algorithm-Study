051eb3ecceb306b0a47ec7cf8f1b39b0c89ea6c3
HyeonMo <gusah34@gmail.com>
2022-02-08 23:06:41 +0900

    0208

---
+++ Algorithm-Study/JavaScript/BOJ_9012_괄호.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+
+const T = input[0]
+input = input.slice(1)
+
+function solution(i) {
+  let stack = []
+  for (inputVal of input[i]) {
+    if (inputVal === '(') {
+      stack.push(inputVal)
+    } else if ( stack.length !== 0 && inputVal === ')') {
+      stack.pop()
+    } else {
+      return 'NO'
+    }
+  }
+  if (stack.length >= 1){
+    return 'NO'
+  } else {
+    return 'YES'
+
+  }
+
+}
+
+
+
+for (let i = 0; i<input.length; i++) {
+  console.log(solution(i))
+
+}
