8f72ff7462a964e59a79fad54c5797e9c8998ed4
HyeonMo <gusah34@gmail.com>
2022-04-07 21:59:12 +0900

    0407

--- Algorithm-Study/JavaScript/[inflearn]JavaScript solving(한국정보올림피아드)/재귀함수와 완전탐색(깊이우선탐색, DFS)/중복순열 구하기.js
+++ Algorithm-Study/JavaScript/[inflearn]JavaScript solving(한국정보올림피아드)/재귀함수와 완전탐색(깊이우선탐색, DFS)/중복순열 구하기.js
@@ -1,15 +1,34 @@
 // 다중 for문 방법
+// function solution(N, M) {
+//   let cnt = 0;
+//   for (let i = 1; i < N + 1; i++) {
+//     for (let j = 1; j < N + 1; j++) {
+//       console.log(i, j);
+//       cnt += 1;
+//     }
+//   }
+//   return cnt;
+// }
+
+// console.log(solution(3, 2));
+
+// 재귀 방법
 function solution(N, M) {
-  let cnt = 0;
-  for (let i = 1; i < N + 1; i++) {
-    for (let j = 1; j < N + 1; j++) {
-      console.log(i, j);
-      cnt += 1;
+  let result = [];
+  let tmp = Array.from({ length: M }, () => 0);
+  function DFS(L) {
+    if (L === M) {
+      result.push(tmp.slice());
+    } else {
+      for (let i = 1; i <= N; i++) {
+        tmp[L] = i;
+        DFS(L + 1);
+      }
     }
   }
-  return cnt;
+
+  DFS(0);
+  return result;
 }
 
 console.log(solution(3, 2));
-
-// 재귀 방법
